63ad746def127afb72a418078daf84a8bead517e
HexMerlin <alec-sweden@hotmail.com>
2025-01-08 12:30:25 +0100

    Add IDfa interface to Automata.Core namespace
    Introduce a new `IDfa` interface that extends `IFsa`.
    This interface includes documentation and defines the `InitialState` property
    and the `State(int fromState)` method for accessing state views.

---
+++ Automata.Core/IDfa.cs
@@ -0,0 +1,19 @@
+﻿namespace Automata.Core;
+
+/// <summary>
+/// A common interface for deterministic finite automata, such as <see cref="Dfa"/> and <see cref="Cfa"/>.
+/// </summary>
+public interface IDfa : IFsa
+{
+    /// <summary>
+    /// Gets the initial state of the DFA.
+    /// </summary>
+    int InitialState { get; }
+
+    /// <summary>
+    /// Gets a read-only view of the state from the specified state.
+    /// </summary>
+    /// <param name="fromState">The state from which to get the state view.</param>
+    /// <returns>A <see cref="StateView"/> representing the state view from the specified state.</returns>
+    StateView State(int fromState);
+}
