53bbfa1dc828042a875107e0bedfeda8255d3b47
HexMerlin <alec-sweden@hotmail.com>
2025-01-08 12:31:58 +0100

    Enhance IAlphabet interface with documentation and features
    Updated the `IAlphabet` interface to include XML documentation for clarity. Added a `Count` property to retrieve the number of symbols. Removed the `GetOrAdd` method, replacing it with a more descriptive indexer for symbol indexing. Documented the `ToString` method and introduced a new `ToStringExpanded` method for detailed representation of symbols and their indices.

--- Automata.Core/Alphabets/IAlphabet.cs
+++ Automata.Core/Alphabets/IAlphabet.cs
@@ -1,18 +1,38 @@
 ﻿namespace Automata.Core.Alphabets;
 
+/// <summary>
+/// Common interface for an alphabet associated with a finite state automaton.
+/// </summary>
 public interface IAlphabet
 {
-    string this[int index] { get; }
-    int this[string symbol] { get; }
+    /// <summary>
+    /// Gets the number of symbols in the alphabet.
+    /// </summary>
     int Count { get; }
 
     /// <summary>
-    /// Gets the index of the specified symbol or adds it if it does not exist.
+    /// Gets the symbol at the specified index.
     /// </summary>
-    /// <param name="symbol">The symbol to get or add.</param>
-    /// <returns>The index of the specified symbol.</returns>
-    public int GetOrAdd(string symbol);
+    /// <param name="index">The index of the symbol.</param>
+    /// <returns>The symbol at the specified index.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the index is out of range.</exception>
+    string this[int index] { get; }
 
+    /// <summary>
+    /// Gets the index of the specified symbol.
+    /// </summary>
+    /// <param name="symbol">The symbol to get the index of.</param>
+    /// <returns>The index of the specified symbol, or <see cref="Constants.InvalidSymbolIndex"/> if the symbol does not exist.</returns>
+    int this[string symbol] { get; }
+
+    /// <summary>
+    /// Returns a string representation of the current alphabet,.
+    /// </summary>
     string ToString();
+
+    /// <summary>
+    /// Returns a string with each symbol and its index, separated by a newline.
+    /// </summary>
+    /// <returns>A string with each symbol and its index, separated by a newline.</returns>
     string ToStringExpanded();
 }
