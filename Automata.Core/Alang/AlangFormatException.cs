be1ae8a40c33bf3ef1a9da09fef8e804a8599dcb
HexMerlin <alec-sweden@hotmail.com>
2025-01-12 14:55:26 +0100

    Refactor AlangFormatException for error handling
    Updated the `AlangFormatException` class to replace the `ErrorType` field with `ErrorReason`. The constructor now accepts `ParseErrorReason`, and a new static method `Assert` has been introduced to conditionally throw exceptions based on a boolean condition, enhancing error handling capabilities.

--- Automata.Core/Alang/AlangFormatException.cs
+++ Automata.Core/Alang/AlangFormatException.cs
@@ -1,7 +1,7 @@
 ﻿namespace Automata.Core.Alang;
 
 /// <summary>
-/// Represents an exception that is thrown when an error occurs during parsing of Alang expressions.
+/// Represents an exception thrown when an error occurs during parsing of Alang expressions.
 /// </summary>
 /// <remarks>
 /// Provides detailed information about the parsing error, including the index in the input where the error occurred and the specific type of error.
@@ -17,19 +17,30 @@ public class AlangFormatException : Exception
     /// <summary>
     /// Type of parsing error that occurred.
     /// </summary>
-    public readonly ParseErrorType ErrorType;
+    public readonly ParseErrorReason ErrorReason;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="AlangFormatException"/> class with the specified error index, error type, and message.
     /// </summary>
+    /// <param name="errorReason">Reason for the parsing error.</param>
     /// <param name="index">Index in the input string where the parsing error occurred.</param>
-    /// <param name="ErrorType">Type of parsing error that occurred.</param>
     /// <param name="message">Error message that explains the reason for the exception.</param>
-    public AlangFormatException(int index, ParseErrorType ErrorType, string message) : base(message)
+    private AlangFormatException(ParseErrorReason errorReason, int index, string message) : base(message)
     {
         this.Index = index;
-        this.ErrorType = ErrorType;
+        this.ErrorReason = errorReason;
     }
+
+    /// <summary>
+    /// Conditionally throws an <see cref="AlangFormatException"/> <c>iff</c> <paramref name="condition"/> is <see langword="false"/>.
+    /// </summary>
+    /// <param name="condition"><c>iff</c> <see langword="false"/> an exception is thrown.</param>
+    /// <param name="errorReason">Reason for the parsing error.</param>
+    /// <param name="index">Index in the input string where the parsing error occurred.</param>
+    /// <param name="message">Error message with details about the exception.</param>
+    /// <returns><see langword="true"/> or throws exception.</returns>
+    public static bool Assert(bool condition, ParseErrorReason errorReason, int index, string message)
+        => condition ? true : throw new AlangFormatException(errorReason, index, message);
 }
 
 
