1ca8e832666c69026aa96c8dab9ae586302d8781
HexMerlin <alec-sweden@hotmail.com>
2025-02-05 11:41:40 +0100

    refactor: update IntSet to implement IReadOnlySet
    - Refactored `IntSet` to encapsulate a `HashSet<int>` and implement `IReadOnlySet<int>`.
    - Introduced private fields for internal storage and cached hash code.
    - Modified constructor to initialize the internal set and compute hash code.
    - Updated `Equals` and `GetHashCode` methods for improved performance.
    - Added methods for containment, overlaps, subsets, and supersets.
    - Adjusted equality and inequality operators to work with the new structure.

--- Automata.Core/IntSet.cs
+++ Automata.Core/IntSet.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace Automata.Core;
 
@@ -6,26 +9,34 @@ namespace Automata.Core;
 /// Immutable set of integers.
 /// </summary>
 [DebuggerDisplay("{ToString()}")]
-public class IntSet : HashSet<int>, IEquatable<IntSet>
+public class IntSet : IEquatable<IntSet>, IReadOnlySet<int>
 {
+    #region Data
+    private readonly HashSet<int> set;
+    private readonly int hashCode;
+
+    #endregion Data
+
     #region Constructors
 
     /// <summary>
     /// Initializes a new instance of the <see cref="IntSet"/> class with the specified elements.
     /// </summary>
     /// <param name="elements">Elements to include in the set.</param>
-    public IntSet(IEnumerable<int> elements) : base(elements) { }
-
-    private IntSet() : base() { }
-
-    public (IntSet intersection, IntSet difference) Split(IEnumerable<int> other)
+    public IntSet(IEnumerable<int> elements)
     {
-        var intersection = new IntSet(this);
-        intersection.IntersectWith(other);
-        var difference = new IntSet(this);
-        difference.IntersectWith(intersection);
-        return (intersection, difference);
+        this.set = new(elements);
+        this.hashCode = ComputeHashCode();
     }
+
+    #endregion Constructors
+
+    #region Accessors
+
+    /// <summary>
+    /// Number of elements in the set.
+    /// </summary>
+    public int Count => set.Count;
 
     /// <summary>
     /// Indicates whether the current set is equal to another set.
@@ -33,7 +44,7 @@ public class IntSet : HashSet<int>, IEquatable<IntSet>
     /// <param name="other">Other set to compare to.</param>
     /// <returns><see langword="true"/> <c>iff</c> the sets are equal.</returns>
     public bool Equals(IntSet? other)
-        => other != null && SetEquals(other);
+        => other is not null && this.hashCode == other.hashCode && set.SetEquals(other.set);
 
     /// <summary>
     /// Indicates whether the current set is equal to another object.
@@ -43,28 +54,93 @@ public class IntSet : HashSet<int>, IEquatable<IntSet>
     public override bool Equals(object? obj)
         => Equals(obj as IntSet);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private int ComputeHashCode()
+    {
+        unchecked
+        {
+            int hash = 1610612741;
+            foreach (int value in this.set)
+            {
+                int mixed = value * 16777619;
+                hash ^= (mixed ^ (mixed >> 16));
+            }
+            return (hash ^ (hash >> 16));
+        }
+    }
+
+
     /// <summary>
     /// Hash code for the current set.
     /// </summary>
     /// <returns>The hash code for the current set.</returns>
-    public override int GetHashCode()
-    {
-        int hash = 17;
-        int count = 0;
-        foreach (int element in this)
-        {
-            hash = hash * 31 + element;
-            if (++count == 10) break;
-        }
-        return hash;
-    }
+    public override int GetHashCode() => this.hashCode;
+
 
     /// <summary>
     /// String that represents the current set.
     /// </summary>
     /// <returns>A string that represents the current set.</returns>
     public override string ToString()
-        => Count <= 10 ? string.Join(", ", this) : string.Join(", ", this.Take(10)) + ", ...";
+        => Count <= 10 ? string.Join(", ", this) : string.Join(", ", this.set.Take(10)) + ", ...";
+
+    /// <summary>
+    /// Indicates whether the current set contains the specified item.
+    /// </summary>
+    /// <param name="item">Item to locate in the set.</param>
+    /// <returns><see langword="true"/> <c>iff</c> the set contains the specified item.</returns>
+    public bool Contains(int item) => this.set.Contains(item);
+
+    /// <summary>
+    /// Indicates whether the current set overlaps with the specified collection.
+    /// </summary>
+    /// <param name="other">Collection to compare to.</param>
+    /// <returns><see langword="true"/> <c>iff</c> the current set overlaps with the specified collection.</returns>
+    public bool Overlaps(IEnumerable<int> other) => set.Overlaps(other);
+
+    /// <summary>
+    /// Indicates whether the current set is a proper subset of the specified collection.
+    /// </summary>
+    /// <param name="other">Collection to compare to.</param>
+    /// <returns><see langword="true"/> <c>iff</c> the current set is a proper subset of the specified collection.</returns>
+    public bool IsProperSubsetOf(IEnumerable<int> other) => this.set.IsProperSubsetOf(other);
+
+    /// <summary>
+    /// Indicates whether the current set is a proper superset of the specified collection.
+    /// </summary>
+    /// <param name="other">Collection to compare to.</param>
+    /// <returns><see langword="true"/> <c>iff</c> the current set is a proper superset of the specified collection.</returns>
+    public bool IsProperSupersetOf(IEnumerable<int> other) => this.set.IsProperSupersetOf(other);
+
+    /// <summary>
+    /// Indicates whether the current set is a subset of the specified collection.
+    /// </summary>
+    /// <param name="other">Collection to compare to.</param>
+    /// <returns><see langword="true"/> <c>iff</c> the current set is a subset of the specified collection.</returns>
+    public bool IsSubsetOf(IEnumerable<int> other) => this.set.IsSubsetOf(other);
+
+    /// <summary>
+    /// Indicates whether the current set is a superset of the specified collection.
+    /// </summary>
+    /// <param name="other">Collection to compare to.</param>
+    /// <returns><see langword="true"/> <c>iff</c> the current set is a superset of the specified collection.</returns>
+    public bool IsSupersetOf(IEnumerable<int> other) => this.set.IsSupersetOf(other);
+
+    /// <summary>
+    /// Indicates whether the current set is equal to the specified collection.
+    /// </summary>
+    /// <param name="other">Collection to compare to.</param>
+    /// <returns><see langword="true"/> <c>iff</c> the current set is equal to the specified collection.</returns>
+    public bool SetEquals(IEnumerable<int> other) => this.set.SetEquals(other);
+
+    /// <summary>
+    /// Returns an enumerator that iterates through the set.
+    /// </summary>
+    /// <returns>An enumerator for the set.</returns>
+    public IEnumerator<int> GetEnumerator() => this.set.GetEnumerator();
+    
+    ///<inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
     /// <summary>
     /// Equality operator.
@@ -72,7 +148,8 @@ public class IntSet : HashSet<int>, IEquatable<IntSet>
     /// <param name="left">Left operand.</param>
     /// <param name="right">Right operand.</param>
     /// <returns><see langword="true"/> <c>iff</c> the operands are equal.</returns>
-    public static bool operator ==(IntSet? left, IntSet? right) => Equals(left, right);
+    public static bool operator ==(IntSet left, IntSet right) 
+        => left.Equals(right);
 
     /// <summary>
     /// Inequality operator.
@@ -80,7 +157,7 @@ public class IntSet : HashSet<int>, IEquatable<IntSet>
     /// <param name="left">Left operand.</param>
     /// <param name="right">Right operand.</param>
     /// <returns><see langword="true"/> <c>iff</c> the operands are not equal.</returns>
-    public static bool operator !=(IntSet? left, IntSet? right) => !Equals(left, right);
+    public static bool operator !=(IntSet left, IntSet right) => !(left == right);
 
     #endregion Accessors
 }
